6e41256cf39af381b8aac92c1953995e15a66da1
Ashwini Shetty <150023632+ashwini-31@users.noreply.github.com>
2024-11-04 19:38:36 +0530

    Time: 147 ms (29.74%) | Memory: 17.1 MB (78.31%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,38 @@
+from typing import List
+from typing import Optional
+
+class Solution:    
+    def minWindow(self, s: str, t: str) -> str:
+        t_counters = {}
+        for c in t:
+            if c not in t_counters.keys():
+                t_counters[c] = 1
+            else:
+                t_counters[c] += 1
+        start = 0
+        end = 0
+        min_window = ''
+        target_len = len(t)        
+        for end in range(len(s)):
+            if s[end] in t_counters.keys() and t_counters[s[end]] > 0:
+                target_len -= 1
+            if s[end] in t_counters.keys():
+                t_counters[s[end]] -= 1
+            while target_len == 0:
+                window_len = end - start + 1
+                if not min_window or window_len < len(min_window):
+                    min_window = s[start:end+1]
+                if s[start] in t_counters.keys():
+                    t_counters[s[start]] += 1
+                if s[start] in t_counters.keys() and t_counters[s[start]] > 0:
+                    target_len += 1
+                start+=1                
+        return min_window
+
+def main():
+    sol = Solution()
+    print('Output:', sol.minWindow("ADOBECODEBANC", "ABC"))
+    print('Expected:', "BANC")
+
+if __name__ == "__main__":
+    main()
